8f8bdb0f993b388b33734e53988e23e6c28e4978
gao-gao <gaoting1996@163.com>
2018-03-09 18:14:49 +0800

    vue route

--- src/router/index.js
+++ src/router/index.js
@@ -3,3 +3,11 @@ import VueRouter from 'vue-router'
 import goods from '../components/goods/goods.vue'
 
 Vue.use(Router)
+
+const routes = [
+    { path: '/goods', component: food }
+]
+
+const router = new VueRouter({
+    routes // （缩写）相当于 routes: routes
+})
