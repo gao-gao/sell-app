99197624526599b5762efdc9f4a636d07afd617f
gao-gao <gaoting1996@163.com>
2018-03-09 23:06:24 +0800

    data undefined

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
 import router from './router/index.js'
 
 import 'common/stylus/index.styl'
+
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
