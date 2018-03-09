fa8ba0dd1320d187d30d306c3906c2611bea8cdb
AloneMonkey <liupeiqing1993@163.com>
2018-03-09 17:22:27 +0800

    sell app

---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header"></div>
+</template>
+
+<script type='text/ecmascript-6'>
+
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+    export default {}
+</style>
