19620704aae7de307a08414724998f43e50b4635
gao-gao <gaoting1996@163.com>
2018-03-11 12:44:05 +0800

    classMap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller='seller'></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -32,7 +32,7 @@ export default {
       res = res.body
       if (res.errno === ERR_OK) {
         this.seller = res.data
-        console.log(this.data)
+        console.log(this.$data)
       }
     })
   },
