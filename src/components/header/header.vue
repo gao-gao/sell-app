178cc3bfc691cd1c451e835458cfb0015c60119b
gao-gao <gaoting1996@163.com>
2018-03-15 20:42:20 +0800

    Duplicate keys

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,20 +13,41 @@
                     {{seller.description}}/{{seller.deliveryTime}}
                 </div>
                 <div v-if='seller.supports' class='support'>
-                    <span class="icon" :class="classMap[seller.supports.type]"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">
                         {{seller.supports[0].description}}
                     </span>
                 </div>
             </div>
+            <div v-if='seller.supports' class='support-count' @click="showDetail">
+                <span class='count'>{{seller.supports.length}}个</span>
+                <i class='icon-keyboard_arrow_right'></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper">
-
+        <div class="bulletin-wrapper" @click="showDetail">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class='icon-keyboard_arrow_right'></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" width='100%' height="100%">
+        </div>
+        <div class="detail" v-show='detailShow'>
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+                    <h1 class="name">{{seller.name}}</h1>
+                    <v-star :size='48' :score='seller.score'></v-star>
+                </div>
+            </div>
+            <div class="detail-close">
+                <i class="icon-close"></i>
+            </div>
         </div>
     </div>
 </template>
 
 <script type='text/ecmascript-6'>
+import star from 'components/star/star'
+
      export default {
         props: {
             seller: {
@@ -35,6 +56,19 @@
         },
         created () {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+        },
+        data () {
+            return {
+                detailShow: false
+            }
+        },
+        methods: {
+            showDetail () {
+                this.detailShow = true
+            }
+        },
+        components: {
+            'v-star': star
         }
     }
 </script>
@@ -43,9 +77,12 @@
 @import '../../common/stylus/mixin.styl'
 
     .header
+        position relative
+        overflow hidden
         color #fff
-        background #000
+        background-color rgba(7,17,27,0.5)
         .content-wrapper
+            position relative
             font-size 0
             padding 24px 12px 18px
             .avatar
@@ -93,4 +130,86 @@
                             bg-image('invoice_1')
                         &.special
                             bg-image('special_1')
+                    .text
+                        font-size 12px
+                        line-height 12px
+                        vertical-align top
+            .support-count
+                position absolute
+                right 12px
+                bottom 14px
+                padding 0 8px
+                height 24px
+                line-height 24px
+                border-radius 14px
+                background-color rgba(0,0,0,0.2)
+                text-align center
+                .count
+                    vertical-align top
+                    font-size 10px
+                .icon-keyboard_arrow_right
+                    font-size 10px
+                    line-height 24px
+                    margin-left 2px
+        .bulletin-wrapper
+           position relative
+           height 28px
+           line-height 28px
+           padding 0 22px
+           white-space nowrap
+           overflow hidden
+           text-overflow ellipsis
+           background-color rgba(7,17,27,0.2)
+           .bulletin-title
+                display inline-block
+                width 22px
+                height 12px
+                vertical-align top
+                margin-top 8px
+                bg-image('bulletin')
+                background-size 22px 12px
+                background-repeat no-repeat
+            .bulletin-text
+                vertical-align top
+                margin 0 4px
+                font-size 10px
+            .icon-keyboard_arrow_right
+                position absolute
+                font-size 10px
+                right 12px
+                top 8px
+                background-color color
+        .background
+            position absolute
+            top 0
+            left 0
+            width 100%
+            height 100%
+            z-index -1
+            filter blur(10px)
+        .detail
+            position fixed
+            top 0px
+            left 0px
+            z-index 100
+            width 100%
+            height 100%
+            overflow auto
+            background rgba(7,17,27,0.8)
+            .detail-wrapper
+                min-height 100%
+                .detail-main
+                    padding-bottom 64px
+                    padding-top 64px
+                    .name
+                        line-height 16px
+                        text-align center
+                        font-size 16px
+                        font-weight 700
+            .detail-close
+                position relative
+                width 32px
+                height 32px
+                margin -64px auto 0 auto
+                font-size 32px
 </style>
